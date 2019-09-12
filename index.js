a3e223596d7c4aa43597936ccfb87c77ad8ae48a
Rajkumar <myidrajkumar@gmail.com>
2019-09-12 20:14:11 +0530

    MongoDB is added

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const { User } = require("./model/user");
 
 mongoose
   .connect(
@@ -10,9 +13,18 @@ mongoose
   .catch(err => console.log("Could not connect to DB: ", err));
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("Hello Ekaa");
+app.post("/api/users/register", (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userData) => {
+        if(err) {
+            return res.json({success: false, err});
+        }
+    });
+  return res.json({success: true}).status(200);
 });
 
 app.listen(5000);
--- model/user.js
+++ model/user.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -32,4 +32,4 @@ const userSchema = mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-export default User;
+module.exports = { User };
